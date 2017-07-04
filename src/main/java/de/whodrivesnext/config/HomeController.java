45c0902fed3d904a5437efaa3a9cf33fc1d10d7e
Frank Scheer <Frank.Scheer@md.de>
2017-07-05 00:03:44 +0200

    Swagger UI

---
+++ src/main/java/de/whodrivesnext/config/HomeController.java
@@ -0,0 +1,30 @@
+//#***************************************************************************
+//# mobilcom IT Entwicklung Source File: HomeController.java
+//# Copyright (c) 1996-2017 by mobilcom-debitel GmbH
+//# All rights reserved.
+//#***************************************************************************
+package de.whodrivesnext.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/********************************************************************
+ * Home redirection to swagger api documentation.
+ */
+@Controller
+public class HomeController
+{
+	//~ Methods ----------------------------------------------------------------------------------------------------------------
+
+	/***************************************
+	 * Weiterleitung von "/" auf "/swagger-ui.html".
+	 *
+	 * @return String zur Weiterleitung auf "/swagger-ui.html"
+	 */
+	@RequestMapping(value = "/")
+	public String index()
+	{
+		return "redirect:swagger-ui.html";
+	}
+}
